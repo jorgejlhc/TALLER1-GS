0d3bb37511fcbc796fe3bec74efe7f26ad71409f
diegoasencio96 <diegoasencio96@gmail.com>
2018-06-02 13:28:09 -0500

    Metodos

--- src/gestionsoftware/GestionSoftware.java
+++ src/gestionsoftware/GestionSoftware.java
@@ -18,46 +18,52 @@ public class GestionSoftware {
      * @param args the command line arguments
      */
     
+    
+    private int ContarPalabras(String texto) {
+        int palabas = texto.split(" ").length;
+        return palabas;
+    }
+    
+    private int NumeroDeCaracteresConEspacios(String texto) {
+        int caracteresconespacio = texto.length();
+        return caracteresconespacio;
+    }
+    
+    
+    private int NumeroDeEspacios(String texto) {
+        int numerodeespacios = texto.split(" ").length-1;
+        return numerodeespacios;
+    }
+    
+    private int NumeroDeCaracteresSinEspacios(String texto) {
+        int caracteressinespacio = NumeroDeCaracteresConEspacios(texto)-NumeroDeEspacios(texto);
+        return caracteressinespacio;
+    }
+    
+    private int NumeroDePalabras(String texto) {
+        int numerodeespacios = texto.split(" ").length;
+        return numerodeespacios;
+    }
+    
     public static void main(String args[]){
-        
+        GestionSoftware gs = new GestionSoftware();
         Scanner reader = new Scanner(System.in);
         String texto = "";   
         do {			
             try {
-                    int palabras = 0;
-                    int caracteresConEspacios = 0;
-                    int caracteresSinEspacios = 0;
-                    int espacios = 0;
-                    
                     System.out.println("Ingrese un una cadena de texto.");
-
                     texto = reader.nextLine();
-                    //texto = "hola mundo ";
-                    caracteresConEspacios = texto.length();                                                          
-                    
-                    System.out.println("La cadena de texto es: " + texto);
-                    
-                    
                     if(texto.isEmpty()){
                         System.out.println("La cadena de texto no puede estar vacia");
                         System.out.println("\n");
                         continue;
                     }
-                   
+                    System.out.println("La cadena de texto es: " + texto);
                     
-                    for(int i = 0; i < caracteresConEspacios; i++){
-                      if (texto.charAt(i) == ' '){
-                            espacios++;
-                        } 
-                      
-                      //System.out.println("\n");
-                    }
-                    
-                    caracteresSinEspacios = caracteresConEspacios - espacios;
-                    
-                    System.out.println("El numero de caracteres en la cadena, incluyendo los espacios es de : " + caracteresConEspacios);
-                    System.out.println("El numero de caracteres en la cadena, sin incluir los espacios es de : " + caracteresSinEspacios);
-                    System.out.println("El numero de espacios en la cadena es de : " + espacios);
+                    System.out.println("El numero de caracteres en la cadena, incluyendo los espacios es de : " + gs.NumeroDeCaracteresConEspacios(texto));
+                    System.out.println("El numero de caracteres en la cadena, sin incluir los espacios es de : " + gs.NumeroDeCaracteresSinEspacios(texto));
+                    System.out.println("El numero de espacios en la cadena es de : " + gs.NumeroDeEspacios(texto));
+                    System.out.println("El numero de palabras en la cadena es de : " + gs.NumeroDePalabras(texto));
                     System.out.println("\n");
                 } catch (InputMismatchException exe){
                   System.out.println("Caracter no valido");
